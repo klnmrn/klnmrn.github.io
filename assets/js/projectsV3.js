e9274e3a4f2d2552f7740915cd2a443f3f94aeed
Marina K <57483760+klnmrn@users.noreply.github.com>
2024-03-17 16:09:27 +0100

    update all

--- assets/js/projectsV3.js
+++ assets/js/projectsV3.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function toggleProjectDetails(project) {
       // Get the active project element
       const activeProject = document.querySelector('.project-details.active');
+
       if (activeProject) {
         // If there is an active project, remove the active class
         activeProject.classList.remove('active');
@@ -22,6 +23,48 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // Toggle the active class on the selected project
       const projectElement = document.getElementById(project);
-      projectElement.classList.toggle('active');
+
+      ///////projectElement.classList.toggle('active');
+
+      // Check if the project element is part of the same origin as the script
+      if (projectElement.ownerDocument.defaultView === window) {
+        projectElement.classList.toggle('active');
+      } else {
+        // If the project element is from a different origin, log a warning message
+        console.warn('The project element is from a different origin.');
+      }
+      
+      const baseUrl = window.location.origin;
+      const currentUrl = window.location.href;
+      
+      if (currentUrl.includes('#')) {
+        // If the current URL contains a project hash, update the URL to index.html#projects
+        window.history.replaceState(null, null, `${baseUrl}/#projects`);
+      } else {
+        // If it doesn't, update the URL to index.html#projects
+        window.history.replaceState(null, null, `${baseUrl}/#${project}`);
+      }}
+
+  document.addEventListener('click', function (event) {
+    const activeProject = document.querySelector('.project-details.active');
+
+    if (event.target.classList.contains('close') && !activeProject.contains(event.target)) {
+      event.preventDefault();
+
+      // Prevent the default behavior of closing the active modal
+      event.stopPropagation();
+
+      // Scroll to the projects list
+      const projectListElement = document.querySelector('#projects'); 
+      projectListElement.scrollIntoView({ behavior: 'smooth' });
+
+      // Update the URL to the base URL with the '#projects' hash
+
+      /////const baseUrl = window.location.protocol + '//' + window.location.host;
+      /////window.history.pushState(null, null, `${baseUrl}#projects`);
+
+      // Update the hash to navigate to #projects
+      window.location.hash = 'projects';
     }
   });
+});
